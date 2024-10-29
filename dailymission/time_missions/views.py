cf182488ee94f41c43c1670a7b9f4749dfbfa547
smiinii <sm020320@naver.com>
2024-10-30 02:09:36 +0900

    host_set

--- dailymission/time_missions/views.py
+++ dailymission/time_missions/views.py
@@ -1,10 +1,20 @@
 # time_missions/views.py
 
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import DailyMission
+from .serializers import DailyMissionSerializer
 
 def home(request):
     # 현재 활성화된 미션 조회
     mission = DailyMission.objects.filter(is_active=True).first()
     print(f"{mission}")
     return render(request, 'time_missions/home.html', {'mission': mission})
+
+
+class ProductListAPI(APIView):
+    def get(self, request):
+        missions = DailyMission.objects.all()
+        serializer = DailyMissionSerializer(missions, many=True)
+        return Response(serializer.data)
