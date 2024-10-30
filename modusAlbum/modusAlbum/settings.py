97711770bd923949111d2f17ebe3acc13128984d
Jaehyun <164978284+jaehyunkor@users.noreply.github.com>
2024-10-31 01:18:46 +0900

    Update settings.py

--- modusAlbum/modusAlbum/settings.py
+++ modusAlbum/modusAlbum/settings.py
@@ -32,6 +32,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://3.37.149.179:8000",
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+
 CSRF_TRUSTED_ORIGINS = (
     'http://localhost:3000',
     'http://127.0.0.1:8000',
