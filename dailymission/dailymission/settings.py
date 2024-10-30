725251d777bfe8e2b5ef68af229910f666df3fac
smiinii <sm020320@naver.com>
2024-10-30 19:58:14 +0900

    퍼블릭 주소 변경

--- dailymission/dailymission/settings.py
+++ dailymission/dailymission/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xn5yvci=mu8j2%hkj9f8l@t-3ei1@g%6v5h@*a1o@kiq*&dgn4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.124.11.104', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['43.203.126.106', 'localhost', '127.0.0.1']
 
 
 # Application definition
