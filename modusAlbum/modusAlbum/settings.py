e6fd851393921052afa7330150e073b118fbc9bb
Jaehyun <164978284+jaehyunkor@users.noreply.github.com>
2024-10-31 00:08:56 +0900

    Update settings.py

--- modusAlbum/modusAlbum/settings.py
+++ modusAlbum/modusAlbum/settings.py
@@ -27,7 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['3.37.149.179', 'localhost', '172.31.12.96', '172.0.0.1']
 
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000/",  # React 앱의 URL
+]
 
 # Application definition
 
