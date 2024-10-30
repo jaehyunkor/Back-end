f210559d7c0b9a8997d38a843f47ed77a8587fc4
Jaehyun <164978284+jaehyunkor@users.noreply.github.com>
2024-10-31 02:22:16 +0900

    Update serializers.py

--- modusAlbum/board/serializers.py
+++ modusAlbum/board/serializers.py
@@ -13,7 +13,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'board', 'created_at']
+        fields = ['id', 'title', 'author', 'content', 'board', 'created_at']
         read_only_fields = ['author', 'created_at']
 
 class ImagePostSerializer(serializers.ModelSerializer):
