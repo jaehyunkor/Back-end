75d9a66252ab38e043a879f63aefd4e6a9d8602c
Jaehyun <164978284+jaehyunkor@users.noreply.github.com>
2024-10-31 00:54:50 +0900

    Update settings.py

--- modusAlbum/modusAlbum/settings.py
+++ modusAlbum/modusAlbum/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'board',
     'accounts',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'modusAlbum.urls'
